76fb092575e6c2fbd1bce0333d9ba618cf745772
JosefaHerrera <josefa@linuxero.cl>
2017-05-24 09:58:08 -0400

    copiando codigo de blanca

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,52 @@
 /*VALIDAR CAMPO DE NUMERO DE TELEFONO*/
-var numero=document.getElementById('botonn');
-numero.addEventListener('click', function(){
-	var num=document.getElementById('phone').value;
+var numero=document.getElementById("botonn");
+numero.addEventListener("click", function(){
+	var num=document.getElementById("phone").value;
 	if(num.length!=9){
 		alert("Debe ingresar un número de 9 dígitos");
 	}else{
-		var text=document.getElementById('phone');
+		var text=document.getElementById("phone");
 		alert("Numero ingresado");
 	}
 });
+
+
+
+
+
+
+
+
+
+/*
+					CODIGO DE BLANCA 
+//Defino el tablero
+var tablero = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+//Ingreso el auto en la posicion 3,5
+tablero[3][5] = "A";
+
+//Obtengo el div en donde se dibujará el tablero
+var divTablero = document.getElementById("tablero");
+
+//Variables temporales
+var fila, casilla;
+
+//Recorro el arreglo para dibujar el tablero
+for(var i = 0; i<tablero.length; i++){
+  fila = document.createElement("div");
+  fila.classList.add("fila");
+  for(var j = 0; j<tablero[i].length; j++){
+    casilla = document.createElement("div");
+    casilla.innerHTML = tablero[i][j];
+    casilla.classList.add("casilla")
+    fila.appendChild(casilla);
+  }
+  divTablero.appendChild(fila);
+}*/
